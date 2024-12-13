03785142f594bc5749688fb4f8c09188801eb33a
alvinbengeorge <alvinallen333@gmail.com>
2024-12-13 08:33:13 +0530

    feat: close button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import DraggableWindow from '@/components/window';
+import React, { useState, useEffect } from "react";
+import DraggableWindow from "@/components/window";
 
 const Home = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
-  const [popup, setPopup] = useState(true);
   const [isActive, setIsActive] = useState(false);
   const [isCooldown, setIsCooldown] = useState(false);
 
@@ -41,6 +40,9 @@ const Home = () => {
   }, [isActive, seconds, minutes, isCooldown]);
 
   const toggle = () => {
+    (
+      document.getElementById("draggableWindow") as HTMLDialogElement
+    ).showModal();
     setIsActive(!isActive);
   };
 
@@ -52,30 +54,47 @@ const Home = () => {
   };
 
   return (
-    <div className={`grid place-items-center duration-500 h-screen ${isCooldown ? 'bg-red-200' : 'bg-green-200'}`}>
-      {popup && <DraggableWindow setPopup={setPopup} />}
-      <div className='text-center'>
+    <div
+      className={`grid place-items-center duration-500 h-screen ${isCooldown ? "bg-red-200" : "bg-green-200"}`}
+    >
+      <DraggableWindow />
+      <div className="text-center">
         <div className="progress-bar w-full bg-gray-200 rounded-full h-4 mb-4">
           <div
-            className={`h-4 rounded-full animate-pulse ${isCooldown ? 'bg-red-700' : 'bg-green-700'}`}
+            className={`h-4 rounded-full animate-pulse ${isCooldown ? "bg-red-700" : "bg-green-700"}`}
             style={{ width: `${((25 - minutes) * 60 + (60 - seconds)) / 15}%` }}
           ></div>
         </div>
-        <div className='grid gap-2'>
+        <div className="grid gap-2">
           <div className="bg-black rounded-2xl p-4">
             <span className="countdown font-mono text-6xl">
-              <span style={{ '--value': minutes } as React.CSSProperties}></span>:
-              <span style={{ '--value': seconds } as React.CSSProperties}></span>
+              <span
+                style={{ "--value": minutes } as React.CSSProperties}
+              ></span>
+              :
+              <span
+                style={{ "--value": seconds } as React.CSSProperties}
+              ></span>
             </span>
           </div>
           <div className="buttons grid gap-2 grid-cols-2">
-            <button onClick={toggle} className={`btn ${isCooldown ? 'btn-error' : 'btn-success'}`}>{isActive ? 'Pause' : 'Start'}</button>
-            <button onClick={reset} className={`btn ${isCooldown ? 'btn-error' : 'btn-success'}`}>Reset</button>
+            <button
+              onClick={toggle}
+              className={`btn ${isCooldown ? "btn-error" : "btn-success"}`}
+            >
+              {isActive ? "Pause" : "Start"}
+            </button>
+            <button
+              onClick={reset}
+              className={`btn ${isCooldown ? "btn-error" : "btn-success"}`}
+            >
+              Reset
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,57 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const DraggableWindow = ({ setPopup }: { setPopup: React.Dispatch<React.SetStateAction<boolean>> }) => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [dragging, setDragging] = useState(false);
-    const [rel, setRel] = useState<{ x: number, y: number } | null>(null);
+const DraggableWindow = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const [rel, setRel] = useState<{ x: number; y: number } | null>(null);
 
-    const onMouseDown = (e: React.MouseEvent) => {
-        if (e.button !== 0) return;
-        const pos = { x: e.pageX - position.x, y: e.pageY - position.y };
-        setRel(pos);
-        setDragging(true);
-        e.stopPropagation();
-        e.preventDefault();
+  const onMouseDown = (e: React.MouseEvent) => {
+    if (e.button !== 0) return;
+    const pos = { x: e.pageX - position.x, y: e.pageY - position.y };
+    setRel(pos);
+    setDragging(true);
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const onMouseUp = () => {
+    setDragging(false);
+  };
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (!dragging) return;
+    setPosition({ x: e.pageX - (rel?.x || 0), y: e.pageY - (rel?.y || 0) });
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
     };
-
-    const onMouseUp = () => {
-        setDragging(false);
-    };
-
-    const onMouseMove = (e: MouseEvent) => {
-        if (!dragging) return;
-        setPosition({ x: e.pageX - (rel?.x || 0), y: e.pageY - (rel?.y || 0) });
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    useEffect(() => {
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-        return () => {
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
-        };
+    setPosition({
+      x: window.innerWidth / 2 - 280,
+      y: window.innerHeight / 2 - 200,
     });
+  });
 
-    return (
-        <div
-            className="absolute"
-            style={{ left: `${position.x}px`, top: `${position.y}px`, zIndex: 1000 }}
-            onMouseDown={onMouseDown}
-        >
-            <div className="bg-base-300 border  rounded-2xl">
-                <div className="bg-base-200 p-2 rounded-2xl">
-                    <div className='grid grid-cols-3'>
-                        <button className='bg-red-600 w-3 h-3 rounded-full hover:bg-red-300 duration-500'></button>
-                    </div>
-                </div>
-                <div className="p-4">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/CS0UAH2UhQw?si=FsvlOCeK0z5XZCqj" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                </div>
-            </div>
+  return (
+    <dialog
+      className="absolute"
+      style={{ left: `${position.x}px`, top: `${position.y}px`, zIndex: 1000 }}
+      onMouseDown={onMouseDown}
+      id="draggableWindow"
+    >
+      <div className="bg-base-300 border  rounded-2xl">
+        <div className="bg-base-200 p-2 rounded-2xl">
+          <div className="grid grid-cols-3">
+            <button
+              className="bg-red-600 w-3 h-3 rounded-full hover:bg-red-300 duration-500"
+              onClick={() => {
+                (
+                  document.getElementById(
+                    "draggableWindow",
+                  ) as HTMLDialogElement
+                ).close();
+              }}
+            ></button>
+          </div>
         </div>
-    );
+        <div className="p-4">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/CS0UAH2UhQw?si=FsvlOCeK0z5XZCqj"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+      </div>
+    </dialog>
+  );
 };
 
 export default DraggableWindow;
